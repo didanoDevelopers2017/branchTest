550720a9fd8931bfe02b0e44c02437aabc0e7e3f
orange <1191066931@qq.com>
2018-04-14 18:31:55 +0800

    at home udpate

--- src/cn/didano/test/HelloWorld.java
+++ src/cn/didano/test/HelloWorld.java
@@ -2,6 +2,6 @@ package cn.didano.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("dfdf");
+        System.out.println("dfdf 20180414 183000 update");
     }
 }
